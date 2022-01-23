dee1aa9569b14553663e8dea281fa1a4af9c0203
erdeiolver <71698306+erdeiolver@users.noreply.github.com>
2022-01-23 15:19:56 +0000

    kód optimalizálása

--- main.py
+++ main.py
@@ -5,35 +5,13 @@
 import random
 
 tarolo = []
-szamlalo = 0
-while True:
+for i in range(4):
     lista = []
-    lista1 = []
-    lista2 = []
-    lista3 = []
-    if szamlalo == 0:
-        tarolo.append(lista1)
-    elif szamlalo == 1:
-        tarolo.append(lista2)
-    elif szamlalo == 2:
-        tarolo.append(lista3)
-    if szamlalo == 3:
-        tarolo.append(lista)
-    elif szamlalo == 5:
-        break
+    tarolo.append(lista)
     for i in range(3):
-        if szamlalo == 0:
-            random_szamok = random.randint(0,25)
-            lista1.append(random_szamok)
-        elif szamlalo == 1:
-            random_szamok = random.randint(0,25)
-            lista2.append(random_szamok)
-        elif szamlalo == 2:
-            random_szamok = random.randint(0,25)
-            lista3.append(random_szamok)
-        elif szamlalo == 3:
-            random_szamok = random.randint(0,25)
-            lista.append(random_szamok)
-    szamlalo += 1
+        randszam = random.randint(0,25)
+        lista.append(randszam)
+
+
 print(tarolo)
     
